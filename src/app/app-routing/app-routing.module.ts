51e7e8ae4aefbfe80ef0d0a253928e34904b3b55
Ahlem FANTAZI <fantaziahlem@gmail.com>
2020-10-14 23:42:43 +0200

    homepage created

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Routes, RouterModule} from "@angular/router";
+import {HomeComponent} from "../home/home.component";
+
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  /*
+  {
+    path:'',
+    //component:LocalisationComponent
+  }
+  */
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from "./app-routing/app-routing.module";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
