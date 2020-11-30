66b3a7b33fe1f3c209b20909ba0af53cb05a30f6
RAMIN <r.erwyn@gmail.com>
2020-11-30 08:43:25 +0100

    Radius slider +Route

--- src/app/carte/localisation/localisation.component.ts
+++ src/app/carte/localisation/localisation.component.ts
@@ -3,8 +3,10 @@ import 'leaflet/dist/leaflet.css';
 import * as L from 'leaflet';
 import {FormControl, NgModel} from '@angular/forms';
 import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
-import {control, Icon, icon, latLng, map, marker, polyline, tileLayer} from 'leaflet';
+import {Browser, circle, Control, control, Icon, icon, latLng, map, marker, polyline, tileLayer} from 'leaflet';
 import scale = control.scale;
+import 'leaflet-easybutton';
+import 'leaflet-easybutton/src/easy-button.css';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent} from '../../app.component';
 import {BrowserModule} from '@angular/platform-browser';
@@ -16,6 +18,8 @@ import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder';
 import * as ELG from 'esri-leaflet-geocoder';
 // @ts-ignore
 import Leaflet from 'leaflet';
+import retina = Browser.retina;
+// import any = jasmine.any;
 
 delete Leaflet.Icon.Default.prototype._getIconUrl;
 
@@ -31,16 +35,18 @@ L.Icon.Default.mergeOptions({
   styleUrls: ['./localisation.component.css']
 })
 
-export class LocalisationComponent implements AfterViewInit, OnInit   {
+export class LocalisationComponent implements AfterViewInit, OnInit {
   searchField: FormControl;
   searches: string[] = [];
   carte;
-  // markerR;
+  marker;
   /* https://www.zupimages.net/*/
-  smallIcon = new L.Icon({ iconUrl: 'https://www.zupimages.net/up/20/43/a73q.png', iconSize: [25, 25],  });
-  ImageIcon = new L.Icon({ iconUrl: 'https://www.zupimages.net/up/20/45/ox4s.png', iconSize: [25, 25],  });
-  GeollocIcon = new L.Icon({ iconUrl: 'https://www.zupimages.net/up/20/47/gk6n.png', iconSize: [25, 25],  });
-  constructor() { }
+  smallIcon = new L.Icon({iconUrl: 'https://www.zupimages.net/up/20/43/a73q.png', iconSize: [25, 25], });
+  ImageIcon = new L.Icon({iconUrl: 'https://www.zupimages.net/up/20/45/ox4s.png', iconSize: [25, 25], });
+  GeollocIcon = new L.Icon({iconUrl: 'https://www.zupimages.net/up/20/47/gk6n.png', iconSize: [25, 25], });
+
+  constructor() {
+  }
 
   ngAfterViewInit(): void {
     this.createMap();
@@ -50,13 +56,13 @@ export class LocalisationComponent implements AfterViewInit, OnInit   {
   createMap() {
     // ajout de pointer marker
     // mettre dans la back
-    const nanterre = L.marker([48.892423, 2.215331], { icon: this.smallIcon } ).bindPopup('Mr Delbot house aka le chef du corona ');
-    const poissy    = L.marker([48.9333, 2.05], { icon: this.smallIcon }).bindPopup('Poissy est une commune française du département des Yvelines en région Île-de-France. La ville possède une longue histoire.ville de ST louis le  Louis IX');
-    const elysee    = L.marker([48.8763, 2.3183], { icon: this.smallIcon }).bindPopup('Le palais de l\'Élysée, dit l\'Élysée, est un ancien hôtel particulier parisien, situé au nᵒ 55 de la rue du Faubourg-Saint-Honoré, dans le 8ᵉ arrondissement de Paris. Il est le siège de la présidence de la République française et la résidence officielle du président de la République depuis la IIᵉ République.');
-    const casino    = L.marker([48.898908, 2.093761], { icon: this.smallIcon }).bindPopup('mon magasin à st germain ');
-    const fleuriste = L.marker([48.9, 2.23], { icon: this.smallIcon }).bindPopup('mon fleuristeUn fleuriste est un artisan spécialisé dans la vente de fleurs et la confection de bouquets de fleurs et d\'assemblages appelés « compositions » de courbevoie ');
-    const eglise = L.marker([48.852968, 2.349902], { icon: this.ImageIcon }).bindPopup('<p>La cathédrale Notre-Dame de Paris, communément appelée Notre-Dame, est l\'un des monuments les plus emblématiques de Paris et de la France. Elle est située sur l\'île de la Cité et est un lieu de culte catholique, siège de l\'archidiocèse de Paris, dédiée à la Vierge Marie</p> <img src=\'https://www.zupimages.net/up/20/45/1c0u.jpg\'>');
-    const louvre = L.marker([48.8592, 2.3417], { icon: this.smallIcon }).bindPopup('Le louvre Leonardo da vinci');
+    const nanterre = L.marker([48.892423, 2.215331], {icon: this.smallIcon}).bindPopup('Mr Delbot house aka le chef du corona ');
+    const poissy = L.marker([48.9333, 2.05], {icon: this.smallIcon}).bindPopup('Poissy est une commune française du département des Yvelines en région Île-de-France. La ville possède une longue histoire.ville de ST louis le  Louis IX');
+    const elysee = L.marker([48.8763, 2.3183], {icon: this.smallIcon}).bindPopup('Le palais de l\'Élysée, dit l\'Élysée, est un ancien hôtel particulier parisien, situé au nᵒ 55 de la rue du Faubourg-Saint-Honoré, dans le 8ᵉ arrondissement de Paris. Il est le siège de la présidence de la République française et la résidence officielle du président de la République depuis la IIᵉ République.');
+    const casino = L.marker([48.898908, 2.093761], {icon: this.smallIcon}).bindPopup('mon magasin à st germain ');
+    const fleuriste = L.marker([48.9, 2.23], {icon: this.smallIcon}).bindPopup('mon fleuristeUn fleuriste est un artisan spécialisé dans la vente de fleurs et la confection de bouquets de fleurs et d\'assemblages appelés « compositions » de courbevoie ');
+    const eglise = L.marker([48.852968, 2.349902], {icon: this.ImageIcon}).bindPopup('<p>La cathédrale Notre-Dame de Paris, communément appelée Notre-Dame, est l\'un des monuments les plus emblématiques de Paris et de la France. Elle est située sur l\'île de la Cité et est un lieu de culte catholique, siège de l\'archidiocèse de Paris, dédiée à la Vierge Marie</p> <img src=\'https://www.zupimages.net/up/20/45/1c0u.jpg\'>');
+    const louvre = L.marker([48.8592, 2.3417], {icon: this.smallIcon}).bindPopup('Le louvre Leonardo da vinci');
 // ajout dans les layers
     const cities = L.layerGroup([nanterre, poissy, elysee, casino, fleuriste, louvre]);
     const monument = L.layerGroup([eglise]);
@@ -70,7 +76,73 @@ export class LocalisationComponent implements AfterViewInit, OnInit   {
         `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
       );
       this.carte = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
-// c'est les maps caret 1-3
+      /* L.easyButton( 'fa-gbp', function circle()
+       L.circle([position.coords.latitude, position.coords.longitude], {radius: 1000}).addTo(this.carte);
+         alert('1km');
+       }).addTo(this.carte);*/
+      const searchControl = ELG.geosearch().addTo(this.carte);
+      const results = L.layerGroup().addTo(this.carte);
+      let markers = [];
+      /*
+            this.carte.on('click', onMapClick);
+            function onMapClick(e){
+              alert('Vous venez de cliquer aux coordonnées ' + e.latlng);
+              const coco = L.marker([e.latitude, e.longitude], { icon: this.smallIcon } ).addTo(this.carte);
+            }
+      */
+      // @ts-ignore
+      const markeree = new L.marker(this.carte.getCenter(), {
+        draggable: true,
+        autoPan: true
+      }).addTo(this.carte);
+
+      searchControl.on('results', (data) => {
+        markers = [];
+        console.log('data', data);
+        results.clearLayers();
+        // several results as several towns with same name (like)
+        for (let i = data.results.length - 1; i >= 0; i--) {
+          const result = data.results[i];
+          const markerR = L.marker(result.latlng);
+          const itineraire1 = L.Routing.control({
+            waypoints: [
+              L.latLng(result.latlng),
+              L.latLng([coords.latitude, coords.longitude])
+            ],
+            useZoomParameter: false,
+            autoRoute: true,
+            routeWhileDragging: true,
+            fitSelectedRoutes: true,
+            router: L.Routing.osrmv1({serviceUrl: 'http://router.project-osrm.org/route/v1'}),
+            // geocoder: L.Control.Geocoder.nominatim({})
+            // router: new L.Routing.graphHopper('62403b36-c15e-4815-b284-8d68590b2bc1');
+          }).addTo(this.carte);
+          // const gph = L.geographPhotos({api_key: '47c9baf779', autoZoomOnAdd: true, query: 'canal'}).addTo(this.carte);
+          // itineraire1.hide().addTo(this.carte);
+          // this.carte.removeControl(itineraire1);
+          //  this.carte.itineraire1.hide();
+          //    L.Routing.itinerary().addTo(this.carte);
+          markers = [...markers];
+          results.addLayer(markerR);
+          console.log(new ELG.ReverseGeocode());
+          markerR.on('click', <LeafletMouseEvent>(e) => {
+            new ELG.ReverseGeocode().latlng(e.latlng).run((error, res) => {
+              if (error) {
+                return;
+              }
+              if (this.marker && this.carte.hasLayer(this.marker)) {
+                this.carte.removeLayer(this.marker);
+              }
+              this.marker = L.marker([result.latitude, result.longitude])
+                .addTo(this.carte)
+                .bindPopup(res.address.Match_addr)
+                .openPopup();
+            });
+          });
+          console.log('markers', markers);
+        }
+      });
+      // c'est les maps caret 1-3
       const carte1 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         minZoom: 3,
         maxZoom: 19,
@@ -89,48 +161,41 @@ export class LocalisationComponent implements AfterViewInit, OnInit   {
         detectRetina: true,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       });
-// on ajoute des lignes pour chaques pts d'interet
-// https://www.liedman.net/leaflet-routing-machine/api/
-      const itineraire1 = L.Routing.control({
-        waypoints: [
-          L.latLng([48.892423, 2.215331]),
-          // L.latLng(poi),
-          L.latLng([coords.latitude, coords.longitude])
-        ],
-        useZoomParameter: false,
-        autoRoute: true,
-        routeWhileDragging: true,
-        // geocoder: L.Control.Geocoder.nominatim()
-      }).addTo(this.carte);
+      const test = (poissy.getLatLng());
+      const testgeo = L.marker([coords.latitude, coords.longitude]);
+      const test1 = (testgeo.getLatLng());
+      const group = L.layerGroup().addTo(this.carte);
+
+      document.getElementById('radius').addEventListener('input', changeRadius);
+      function changeRadius(event) {
+         const newRadius = event.target.value;
+        // tslint:disable-next-line:only-arrow-functions
+         group.eachLayer(function(layer) {
+          if (layer instanceof L.Circle) {
+            layer.setRadius(newRadius); // obtenir le rayon
+          }
+        });
+       }
+      const circle = L.circle(test1, {
+        radius: 1000,
+      }).addTo(group);
+
+      // tslint:disable-next-line:no-shadowed-variable
+      const marker = L.marker(test1).addTo(group);
+       // https://www.liedman.net/leaflet-routing-machine/api/
       // calcule la distance entre 2 marker
-      // const distance = poissy.getLatLng().distanceTo(elysee.getLatLng());
-      // alert(distance);
-
-      /* Si on a des donnees dans la base
-          L.routing.control.({
-            waypoints: [null
-            ]
-          })
-
-         ou si on a plusieurs pts
-
-          L.routing.control.setWaypoints([
-            L.latLng(lat1, lon1),
-            L.latLng(lat2, lon2)
-          ]);
-          */
       const baseMaps = {carte1, carte2, carte3};
       const overlayMaps = {cities, monument};
       const Geomarker = L.marker([coords.latitude, coords.longitude], {icon: this.GeollocIcon}).addTo(this.carte);
       Geomarker.bindPopup('<b>Ici</b>').openPopup();
       // faire pour les images
-      L.control.layers(baseMaps, overlayMaps).addTo(this.carte).setPosition('bottomleft');
+      const controller = L.control.layers(baseMaps, overlayMaps).addTo(this.carte).setPosition('bottomleft');
       scale().addTo(this.carte);
     });
     this.watchPosition();
   }
   // tslint:disable-next-line:typedef
-  watchPosition(){
+  watchPosition() {
     const desLat = 0;
     const desLon = 0;
     const id = navigator.geolocation.watchPosition(
@@ -165,5 +230,4 @@ export class LocalisationComponent implements AfterViewInit, OnInit   {
         this.searches.push(term);
       });
   }
-
 }
