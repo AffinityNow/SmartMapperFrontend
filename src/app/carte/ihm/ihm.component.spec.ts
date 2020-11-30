e5e3ecadc106a5427ed9c9a5b5fa55e68bcc3716
Avi Assayag <avi_assayag22@hotmail.fr>
2020-11-30 22:31:39 +0100

    Tests fix V1

--- src/app/carte/ihm/ihm.component.spec.ts
+++ src/app/carte/ihm/ihm.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IHMComponent } from './ihm.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('IHMComponent', () => {
   let component: IHMComponent;
@@ -8,6 +9,7 @@ describe('IHMComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ IHMComponent ]
     })
     .compileComponents();
--- src/app/shared/service/point-interet.service.spec.ts
+++ src/app/shared/service/point-interet.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PointInteretService } from './point-interet.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('PointInteretService', () => {
   let service: PointInteretService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClientTestingModule]});
     service = TestBed.inject(PointInteretService);
   });
 
