0048ff4ac176676fbc7623443993aadc7ab33a69
Ahlem FANTAZI <fantaziahlem@gmail.com>
2020-12-07 12:32:34 +0100

    Adding HTTP Error Handling with RxJS catchError() & HttpClient

--- src/app/carte/ihm/ihm.component.ts
+++ src/app/carte/ihm/ihm.component.ts
@@ -1,12 +1,15 @@
 // Ahlem
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 import {Categories, PointInteret} from '../../shared/model/pointInteret';
 import {PointInteretService} from '../../shared/service/point-interet.service';
-import {Component, AfterViewInit, OnInit} from '@angular/core';
-import * as L from 'leaflet';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 // Erwyn
 // @ts-ignore
-import Leaflet from 'leaflet';
+import * as L from 'leaflet';
+// @ts-ignore
+import Leaflet, {control} from 'leaflet';
 import {FormControl} from '@angular/forms';
 import 'leaflet-routing-machine';
 import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css';
@@ -14,22 +17,12 @@ import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder';
 import * as ELG from 'esri-leaflet-geocoder';
 import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 import 'leaflet/dist/leaflet.css';
-import {Browser, circle, Control, control, Icon, icon, latLng, map, marker, polyline, tileLayer} from 'leaflet';
-import scale = control.scale;
 import 'leaflet-easybutton';
 import 'leaflet-easybutton/src/easy-button.css';
-import 'leaflet-routing-machine';
-import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css';
-import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder';
-import 'leaflet-routing-machine';
 import 'leaflet-control-geocoder';
-import layers = control.layers;
-import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css';
-import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
-import 'leaflet-routing-machine';
 import 'style-loader!esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css';
-// import any = jasmine.any;
+import scale = control.scale;
 
 
 // Ahlem
@@ -55,22 +48,24 @@ L.Icon.Default.mergeOptions({
   templateUrl: './ihm.component.html',
   styleUrls: ['./ihm.component.css']
 })
-export class IHMComponent implements OnInit, AfterViewInit {
+export class RecherchPointInteretComponent implements OnInit, OnDestroy, AfterViewInit {
   // Erwyn
   searchField: FormControl;
   searches: string[] = [];
   carte;
   /* https://www.zupimages.net/*/
-  smallIcon = new L.Icon({iconUrl: 'https://www.zupimages.net/up/20/43/a73q.png', iconSize: [25, 25], });
-  ImageIcon = new L.Icon({iconUrl: 'https://www.zupimages.net/up/20/45/ox4s.png', iconSize: [25, 25], });
-  GeollocIcon = new L.Icon({iconUrl: 'https://www.zupimages.net/up/20/47/gk6n.png', iconSize: [25, 25], });
+  smallIcon = new L.Icon({iconUrl: 'https://www.zupimages.net/up/20/43/a73q.png', iconSize: [25, 25],});
+  ImageIcon = new L.Icon({iconUrl: 'https://www.zupimages.net/up/20/45/ox4s.png', iconSize: [25, 25],});
+  GeollocIcon = new L.Icon({iconUrl: 'https://www.zupimages.net/up/20/47/gk6n.png', iconSize: [25, 25],});
 
 // *******************Ahlem*****************************************
-  pointInteretList: string[];
+  destroy$: Subject<boolean> = new Subject<boolean>();
   pointInteretSelectionnes: string[];
   caterogies: any[];
-  displayAddressList = false;
   pointInteretCurrent: PointInteret[] = [];
+  displayAddressList = false;
+  poiAvailable = false;
+  map;
   marker;
 
   constructor(private pointInteretService: PointInteretService) {
@@ -78,21 +73,31 @@ export class IHMComponent implements OnInit, AfterViewInit {
 
   getCurrentPointInteret(categorie: string): void {
     this.displayAddressList = true;
-    this.pointInteretService.loadPointInteretByCategorie(categorie).subscribe(res => this.pointInteretCurrent = res);
+    this.poiAvailable = true;
+    this.pointInteretService.loadPointInteretByCategorie(categorie).pipe(takeUntil(this.destroy$))
+      .subscribe(res => {
+        this.pointInteretCurrent = res;
+        this.poiAvailable = false;
+      });
   }
 
-  // tslint:disable-next-line:typedef
   drawMarker(event) {
     const pointInteretSelectionnes = event.value;
     console.log('pointInteretSelectionnes : ' + pointInteretSelectionnes);
     const pids = pointInteretSelectionnes;
     console.log('positions : ' + pids);
-    // tslint:disable-next-line:max-line-length
-    pids.forEach(pid => L.marker([pid.coordonnes.latitude, pid.coordonnes.longitude], {icon: greenIcon}).addTo(this.carte).bindPopup(pid.description).openPopup());
+    pids.forEach(pid => L.marker([pid.coordonnes.latitude, pid.coordonnes.longitude], {icon: greenIcon})
+      .addTo(this.carte).bindPopup(pid.description).openPopup());
   }
 
   ngAfterViewInit(): void {
     this.createMap();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
   }
 
   ngOnInit(): void {
@@ -114,18 +119,6 @@ export class IHMComponent implements OnInit, AfterViewInit {
 
   // tslint:disable-next-line:typedef
   createMap() {
-    // ajout de pointer marker
-    // mettre dans la back
-    const nanterre = L.marker([48.892423, 2.215331], {icon: this.smallIcon}).bindPopup('Mr Delbot house aka le chef du corona ');
-    const poissy = L.marker([48.9333, 2.05], {icon: this.smallIcon}).bindPopup('Poissy est une commune française du département des Yvelines en région Île-de-France. La ville possède une longue histoire.ville de ST louis le  Louis IX');
-    const elysee = L.marker([48.8763, 2.3183], {icon: this.smallIcon}).bindPopup('Le palais de l\'Élysée, dit l\'Élysée, est un ancien hôtel particulier parisien, situé au nᵒ 55 de la rue du Faubourg-Saint-Honoré, dans le 8ᵉ arrondissement de Paris. Il est le siège de la présidence de la République française et la résidence officielle du président de la République depuis la IIᵉ République.');
-    const casino = L.marker([48.898908, 2.093761], {icon: this.smallIcon}).bindPopup('mon magasin à st germain ');
-    const fleuriste = L.marker([48.9, 2.23], {icon: this.smallIcon}).bindPopup('mon fleuristeUn fleuriste est un artisan spécialisé dans la vente de fleurs et la confection de bouquets de fleurs et d\'assemblages appelés « compositions » de courbevoie ');
-    const eglise = L.marker([48.852968, 2.349902], {icon: this.ImageIcon}).bindPopup('<p>La cathédrale Notre-Dame de Paris, communément appelée Notre-Dame, est l\'un des monuments les plus emblématiques de Paris et de la France. Elle est située sur l\'île de la Cité et est un lieu de culte catholique, siège de l\'archidiocèse de Paris, dédiée à la Vierge Marie</p> <img src=\'https://www.zupimages.net/up/20/45/1c0u.jpg\'>');
-    const louvre = L.marker([48.8592, 2.3417], {icon: this.smallIcon}).bindPopup('Le louvre Leonardo da vinci');
-    // ajout dans les layers
-    const cities = L.layerGroup([nanterre, poissy, elysee, casino, fleuriste, louvre]);
-    const monument = L.layerGroup([eglise]);
     if (!navigator.geolocation) {
       console.log('location is not supported');
     }
@@ -136,20 +129,11 @@ export class IHMComponent implements OnInit, AfterViewInit {
         `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
       );
       this.carte = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
-      /* L.easyButton( 'fa-gbp', function circle()
-       L.circle([position.coords.latitude, position.coords.longitude], {radius: 1000}).addTo(this.carte);
-         alert('1km');
-       }).addTo(this.carte);*/
+
       const searchControl = ELG.geosearch().addTo(this.carte);
       const results = L.layerGroup().addTo(this.carte);
       let markers = [];
-      /*
-            this.carte.on('click', onMapClick);
-            function onMapClick(e){
-              alert('Vous venez de cliquer aux coordonnées ' + e.latlng);
-              const coco = L.marker([e.latitude, e.longitude], { icon: this.smallIcon } ).addTo(this.carte);
-            }
-      */
+
       // @ts-ignore
       const markeree = new L.marker(this.carte.getCenter(), {
         draggable: true,
@@ -159,7 +143,6 @@ export class IHMComponent implements OnInit, AfterViewInit {
         markers = [];
         console.log('data', data);
         results.clearLayers();
-        // several results as several towns with same name (like)
         for (let i = data.results.length - 1; i >= 0; i--) {
           const result = data.results[i];
           const markerR = L.marker(result.latlng);
@@ -173,18 +156,16 @@ export class IHMComponent implements OnInit, AfterViewInit {
             routeWhileDragging: true,
             fitSelectedRoutes: true,
             router: L.Routing.osrmv1({serviceUrl: 'http://router.project-osrm.org/route/v1'}),
-            // geocoder: L.Control.Geocoder.nominatim({})
             // router: new L.Routing.graphHopper('62403b36-c15e-4815-b284-8d68590b2bc1');
           }).addTo(this.carte);
           // tslint:disable-next-line:typedef only-arrow-functions
-          itineraire1.on('routesfound', function(e) {
+          itineraire1.on('routesfound', function (e) {
             const routes = e.routes;
             const summary = routes[0].summary;
-            if (summary.totalDistance > 20000){
+            if (summary.totalDistance > 20000) {
               circle.setStyle({color: 'red'});
             }
           });
-          // const gph = L.geographPhotos({api_key: '47c9baf779', autoZoomOnAdd: true, query: 'canal'}).addTo(this.carte);
           markers = [...markers];
           results.addLayer(markerR);
           console.log(new ELG.ReverseGeocode());
@@ -217,13 +198,12 @@ export class IHMComponent implements OnInit, AfterViewInit {
         router: L.Routing.osrmv1({serviceUrl: 'http://router.project-osrm.org/route/v1'}),
         // @ts-ignore
         geocoder: L.Control.Geocoder.nominatim({})
-        // router: new L.Routing.graphHopper('62403b36-c15e-4815-b284-8d68590b2bc1');
       }).addTo(this.carte);
       // tslint:disable-next-line:only-arrow-functions typedef
-      itineraire2.on('routesfound', function(e) {
+      itineraire2.on('routesfound', function (e) {
         const routes = e.routes;
         const summary = routes[0].summary;
-        if (summary.totalDistance > 20000){ // changeRadius(test1)
+        if (summary.totalDistance > 20000) { // changeRadius(test1)
           circle.setStyle({color: 'red'});
         }
       });
@@ -246,7 +226,6 @@ export class IHMComponent implements OnInit, AfterViewInit {
         detectRetina: true,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       });
-      const test = (poissy.getLatLng());
       const testgeo = L.marker([coords.latitude, coords.longitude]);
       const test1 = (testgeo.getLatLng());
       const group = L.layerGroup().addTo(this.carte);
@@ -257,7 +236,7 @@ export class IHMComponent implements OnInit, AfterViewInit {
       function changeRadius(event) {
         const newRadius = event.target.value;
         // tslint:disable-next-line:only-arrow-functions typedef
-        group.eachLayer(function(layer) {
+        group.eachLayer(function (layer) {
           if (layer instanceof L.Circle) {
             layer.setRadius(newRadius); // obtenir le rayon
           }
@@ -274,11 +253,10 @@ export class IHMComponent implements OnInit, AfterViewInit {
       // https://www.liedman.net/leaflet-routing-machine/api/
       // calcule la distance entre 2 marker
       const baseMaps = {carte1, carte2, carte3};
-      const overlayMaps = {cities, monument};
       const Geomarker = L.marker([coords.latitude, coords.longitude], {icon: this.GeollocIcon}).addTo(this.carte);
       Geomarker.bindPopup('<b>Vous êtes ici</b>').openPopup();
       // faire pour les images
-      const controller = L.control.layers(baseMaps, overlayMaps).addTo(this.carte).setPosition('bottomleft');
+      const controller = L.control.layers(baseMaps).addTo(this.carte).setPosition('bottomleft');
       scale().addTo(this.carte);
     });
     this.watchPosition();
@@ -307,6 +285,4 @@ export class IHMComponent implements OnInit, AfterViewInit {
       }
     );
   }
-
-
 }
